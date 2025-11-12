8c075234db82597f0f94c26543cbcdb2e12de032
Scott Bellware <sbellware@gmail.com>
2025-11-12 11:21:09 -0800

    Set test comments and details are corrected

--- test/automated/set.rb
+++ test/automated/set.rb
@@ -4,18 +4,20 @@ context "Set" do
   control_var_name = "TEST_#{SecureRandom.hex}"
   control_value = SecureRandom.hex
 
+  comment "Environment Variable: #{control_var_name.inspect}"
+
   EnvVar.set(control_var_name, control_value)
 
   begin
     environment_value = ENV.fetch(control_var_name)
 
     context do
-      detail "Environment variable: #{environment_value.inspect}"
-        detail "Control: #{control_var_name.inspect}"
+      comment "Value: #{environment_value.inspect}"
+      detail "Control: #{control_value.inspect}"
 
-        test do
-          assert(environment_value == control_value)
-        end
+      test do
+        assert(environment_value == control_value)
+      end
     end
   ensure
     ENV.delete(control_var_name)
