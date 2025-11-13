3887147f43883eedcd26ff03d5e89412c68396cc
Scott Bellware <sbellware@gmail.com>
2025-11-13 11:23:55 -0800

    Test instrumentation is corrected

--- test/automated/fetch/previously_not_set.rb
+++ test/automated/fetch/previously_not_set.rb
@@ -5,7 +5,7 @@ context "Fetch" do
     control_var_name = "TEST_#{SecureRandom.hex}"
 
     context do
-      detail "Environment variable: #{ENV[control_var_name].inspect}"
+      comment "Environment variable: #{ENV[control_var_name].inspect}"
 
       assert_raises(KeyError) do
         EnvVar.fetch(control_var_name)
