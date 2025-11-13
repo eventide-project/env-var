625af9bd213e35080723c4d9962c16617d731a70
Pablo Vicente <vicapa99@gmail.com>
2025-11-13 08:17:53 +0100

    Unset test comments and details are corrected

--- test/automated/unset.rb
+++ test/automated/unset.rb
@@ -4,13 +4,14 @@ context "Unset" do
   control_var_name = "TEST_#{SecureRandom.hex}"
   control_value = SecureRandom.hex
 
+  comment "Environment Variable: #{control_var_name.inspect}"
+
   ENV[control_var_name] = control_value
 
   EnvVar.unset(control_var_name)
 
   context do
-    detail "Environment variable: #{ENV[control_var_name].inspect}"
-    detail "Control: #{control_var_name.inspect}"
+    detail "Value: #{ENV[control_var_name].inspect}"
 
     test do
       assert(ENV[control_var_name].nil?)
