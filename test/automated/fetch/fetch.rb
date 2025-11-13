de3b041b46139877274d53f90616c7a13552925b
Pablo Vicente <vicapa99@gmail.com>
2025-11-13 08:10:33 +0100

    Fetch test comments and details are corrected

--- test/automated/fetch/fetch.rb
+++ test/automated/fetch/fetch.rb
@@ -4,12 +4,14 @@ context "Fetch" do
   control_var_name = "TEST_#{SecureRandom.hex}"
   control_value = SecureRandom.hex
 
+  comment "Environment Variable: #{control_var_name.inspect}"
+
   ENV[control_var_name] = control_value
 
   environment_value = EnvVar.fetch(control_var_name)
 
   context do
-    detail "Environment variable: #{ENV[control_var_name].inspect}"
+    comment "Value: #{ENV[control_var_name].inspect}"
     detail "Control: #{environment_value.inspect}"
 
     test do
