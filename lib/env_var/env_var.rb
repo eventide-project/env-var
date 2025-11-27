970bada588eb93885919a91623a1d878774150d3
Scott Bellware <sbellware@gmail.com>
2025-11-27 12:11:27 -0800

    Methods are ordered least destructive to most destructive

--- lib/env_var/env_var.rb
+++ lib/env_var/env_var.rb
@@ -1,20 +1,20 @@
 module EnvVar
   Error = Class.new(StandardError)
 
-  def self.set(name, value)
-    ENV[name] = value
-  end
-
-  def self.unset(name)
-    ENV.delete(name)
-  end
-
   def self.get(name)
     ENV[name]
   end
 
   def self.fetch(name)
     ENV.fetch(name)
+  end
+
+  def self.set(name, value)
+    ENV[name] = value
+  end
+
+  def self.unset(name)
+    ENV.delete(name)
   end
 
   def self.push(name_or_hash, value=nil, &action)
