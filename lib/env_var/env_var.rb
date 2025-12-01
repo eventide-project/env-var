a82008646731ed32b7b97d6bf23a69cd37464fc0
Scott Bellware <sbellware@gmail.com>
2025-12-01 12:01:46 -0800

    Logging uses deferred evaluation of log text

--- lib/env_var/env_var.rb
+++ lib/env_var/env_var.rb
@@ -6,33 +6,33 @@ module EnvVar
   end
 
   def self.get(name)
-    logger.trace("Getting environment variable (Name: #{name.inspect})")
+    logger.trace { "Getting environment variable (Name: #{name.inspect})" }
     value = ENV[name]
-    logger.debug("Got environment variable (Name: #{name.inspect}, Value: #{value.inspect})")
+    logger.debug { "Got environment variable (Name: #{name.inspect}, Value: #{value.inspect})" }
 
     value
   end
 
   def self.fetch(name)
-    logger.trace("Fetching environment variable (Name: #{name.inspect})")
+    logger.trace { "Fetching environment variable (Name: #{name.inspect})" }
     value = ENV.fetch(name)
-    logger.debug("Fetched environment variable (Name: #{name.inspect}, Value: #{value.inspect})")
+    logger.debug { "Fetched environment variable (Name: #{name.inspect}, Value: #{value.inspect})" }
 
     value
   end
 
   def self.set(name, value)
-    logger.trace("Setting environment variable (Name: #{name.inspect}, Value: #{value.inspect})")
+    logger.trace { "Setting environment variable (Name: #{name.inspect}, Value: #{value.inspect})" }
     ENV[name] = value
-    logger.debug("Set environment variable (Name: #{name.inspect}, Value: #{value.inspect})")
+    logger.debug { "Set environment variable (Name: #{name.inspect}, Value: #{value.inspect})" }
 
     value
   end
 
   def self.unset(name)
-    logger.trace("Unsetting environment variable (Name: #{name.inspect})")
+    logger.trace { "Unsetting environment variable (Name: #{name.inspect})" }
     value = ENV.delete(name)
-    logger.debug("Unset environment variable (Name: #{name.inspect}, Value: #{value.inspect})")
+    logger.debug { "Unset environment variable (Name: #{name.inspect}, Value: #{value.inspect})" }
 
     value
   end
@@ -43,10 +43,10 @@ module EnvVar
     hash = nil
     if name_or_hash.respond_to?(:to_hash)
       hash = name_or_hash.to_hash
-      logger.trace("Pushing environment variables (#{hash.inspect})")
+      logger.trace { "Pushing environment variables (#{hash.inspect})" }
     else
       name = name_or_hash
-      logger.trace("Pushing environment variable (Name: #{name.inspect}, Value: #{value.inspect})")
+      logger.trace { "Pushing environment variable (Name: #{name.inspect}, Value: #{value.inspect})" }
       hash = { name => value }
     end
 
@@ -61,7 +61,7 @@ module EnvVar
     begin
       action.call
 
-      logger.debug("Pushed environment variables (#{hash.inspect})")
+      logger.debug { "Pushed environment variables (#{hash.inspect})" }
 
       original_values
     ensure
