73c0e54b5061ec9410b2e230b992dbb7ff27ea4f
Scott Bellware <sbellware@gmail.com>
2026-03-06 10:07:57 -0800

    Gem installation no longer uses the path option

--- install_gems.sh
+++ install_gems.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/env bash
 
+echo
+echo "Installing gems to ./gems"
+echo '= = ='
+echo
+
 echo "Removing gems directory..."
-rm -rf gems
+rm -rf ./gems
 echo "Removing .bundle directory..."
 rm -rf .bundle
 echo "Removing Gemfile.lock file..."
 rm -f Gemfile.lock
+
 echo
+echo "Setting installation path"
+echo "- - -"
 
-echo "Installing gems to ./gems"
-echo '= = ='
-
-cmd="bundle install --standalone --path=./gems"
+cmd="bundle config set --local path ./gems"
 
 echo $cmd
 ($cmd)
 
-echo '- - -'
-echo '(done)'
+
+echo
+echo "Installing"
+echo "- - -"
+
+cmd="bundle install --standalone"
+
+echo $cmd
+($cmd)
+
+echo "= = ="
+echo "Done"
 echo
